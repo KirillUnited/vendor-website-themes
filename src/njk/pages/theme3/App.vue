008a19b88d49b011fb9fef64846732f3300b0eb1
k.dubov <example@example.com>
2021-08-20 17:54:38 +0300

    [theme3] Add components and update App
    Add Header, Footer
    
    Add Gallery

--- src/njk/pages/theme3/App.vue
+++ src/njk/pages/theme3/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <section class="vw-section vw-gallery" id="galleryModal">
-    <div class="vw-section-header">
-      <div class="vw-container">
-        <h1>{{message}}</h1>
-      </div>
-    </div>
-    <div class="vw-section-content">
-      <div class="vw-container">
-        <ul class="vw-gallery-list list-style" data-prop="lightgallery">
-          <li v-for="item in items" :key="item" class="vw-gallery-list-item has-img-fit">
-             <img :src="item" alt="">
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
+  <div class="vw-wrapper">
+    <Header></Header>
+    <main class="vw-content">
+      <section class="vw-section vw-gallery" id="galleryModal">
+        <div class="vw-section-header">
+          <div class="vw-container">
+            <h1>{{ message }}</h1>
+          </div>
+        </div>
+        <div class="vw-section-content">
+          <div class="vw-container">
+            <ul class="vw-gallery-list list-style" data-prop="lightgallery">
+              <li
+                v-for="item in items"
+                :key="item"
+                class="vw-gallery-list-item has-img-fit"
+              >
+                <img :src="item" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Footer from './Footer.vue';
+import Header from './Header.vue';
 export default {
+  components: { Header, Footer },
   name: "App",
   data() {
     return {
       message: "Gallery",
-      items: ["https://assets.codepen.io/12005/windmill.jpg", "https://assets.codepen.io/12005/suspension-bridge.jpg", "https://assets.codepen.io/12005/sunset.jpg"]
+      items: [
+        "https://assets.codepen.io/12005/windmill.jpg",
+        "https://assets.codepen.io/12005/suspension-bridge.jpg",
+        "https://assets.codepen.io/12005/sunset.jpg",
+      ],
     };
   },
 };
---
+++ src/njk/pages/theme3/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="vw-header">
+    <div class="vw-container">
+      <input
+        type="checkbox"
+        name="menu-active"
+        id="menu-active"
+        hidden="hidden"
+      />
+
+      <div class="vw-header-navbar">
+        <label class="vw-header-burger hidden-gt-xs" for="menu-active">
+          <div class="lines">
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="line-3"></div>
+          </div>
+        </label>
+
+        <div class="vw-header-logo vw-header-item has-img-fit">
+          <img src="../../../img/vw-logo.png" alt="logo" />
+          <img
+            class="logo-invert"
+            src="../../../img/vw-logo-invert.png"
+            alt="logo"
+          />
+        </div>
+        <nav class="vw-header-item has-menu">
+          <ul class="vw-header-menu list-style">
+            <li
+              v-for="link in links"
+              :key="link.id"
+              class="vw-header-menu-item"
+            >
+              <a
+                class="vw-header-menu-link vw-button vw-button-invert"
+                :href="'#' + link.id"
+              >
+                {{ link.title }}
+              </a>
+            </li>
+
+            <li class="vw-header-menu-item">
+              <button
+                class="vw-header-menu-link vw-button"
+                data-toggle="modal"
+                data-target="#contactModal"
+              >
+                contact
+              </button>
+              <!-- {% include "../components/_modal.njk" %} -->
+            </li>
+          </ul>
+        </nav>
+        <div class="vw-header-item has-social">
+          <!-- {{ social.socialList("is-colored") }} -->
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { title: "about me", id: "about" },
+        { title: "gallery", id: "gallery" },
+        { title: "pricing", id: "pricing" },
+        { title: "testimonials", id: "reviews" },
+        { title: "blog", id: "posts" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
