9362d9834fb6384fe725039a86eb96bec93b8e91
k.dubov <example@example.com>
2021-09-02 18:25:04 +0300

    Add pug

--- src/njk/pages/theme3/components/ArticleCard.vue
+++ src/njk/pages/theme3/components/ArticleCard.vue
@@ -1,22 +1,34 @@
-<template>
-  <article class="vw-post-card">
-    <div class="vw-post-card-img has-img-fit">
-      <img :src="image" alt="" />
-    </div>
-    <div class="vw-post-card-content">
-      <h3 class="vw-post-card-title">
-        {{ title }}
-      </h3>
-      <p class="vw-post-card-metadata">
-        <span>3 minute read</span>
-        <span class="date">January 25, 2018</span>
-      </p>
-      <p class="vw-post-card-desc hidden-xs">
-        {{ body }}
-      </p>
-      <a href="#" class="vw-button">read</a>
-    </div>
-  </article>
+<template lang="pug">
+article.vw-post-card
+  .vw-post-card-img.has-img-fit
+    img(:src="image", :alt="title")
+  .vw-post-card-content
+    h3.vw-post-card-title
+      | {{ title }}
+    p.vw-post-card-metadata
+      span 3 minute read
+      span.date January 25, 2018
+    p.vw-post-card-desc.hidden-xs
+      | {{ body }}
+    a.vw-button(href="#") read
+  //- <article class="vw-post-card">
+  //-   <div class="vw-post-card-img has-img-fit">
+  //-     <img :src="image" alt="" />
+  //-   </div>
+  //-   <div class="vw-post-card-content">
+  //-     <h3 class="vw-post-card-title">
+  //-       {{ title }}
+  //-     </h3>
+  //-     <p class="vw-post-card-metadata">
+  //-       <span>3 minute read</span>
+  //-       <span class="date">January 25, 2018</span>
+  //-     </p>
+  //-     <p class="vw-post-card-desc hidden-xs">
+  //-       {{ body }}
+  //-     </p>
+  //-     <a href="#" class="vw-button">read</a>
+  //-   </div>
+  //- </article>
 </template>
 
 <script>
